3720c28cbd73b3e021891ab2d4410070fab55df6
Johanna L <johannarleal@gmail.com>
2021-02-10 23:33:15 -0500

    Started mocking up HTML template

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ const questions = [
 ];
 
 // FUNCTIONS ==================================
+
 const getManager = () => {
     let managerQuestions = questions.concat(
                 {
@@ -185,87 +186,6 @@ const getIntern = () => {
 const renderHTML = () => {
 
 };
-
-// writeUserInfo - takes user responses and writes a README.md file
-// const writeUserInfo = (userResponses) => {
-//   // Build a string with user responses.
-//   const content = formatReadMe(userResponses);
-
-//   // Write the generated content to a file. If there is an error writing to the file, console.log the error message received.
-//   // Else, console.log an success message.
-//   // NOTE: FOR TESTING PURPOSES I HAVE NAMED THE OUTPUT FILE README_TEST.MD SO AS NOT TO OVERWRITE MY ACTUAL README.MD FILE.
-//   fs.writeFile("readme_test.md", content, (err) => err ? console.error(err) : console.log("File was successly generated!"));
-// }
-
-// formatReadMe - takes the user responses to the prompts and creates a string with the user responses inserted as 
-// template literals in the appropriate locations within the output string.
-// const formatReadMe = (userResponses) => {
-//     let link = "";
-
-//     // Find the selected badge link.
-//     switch (userResponses.license)  {
-//         case "Apache License 2.0":
-//             link = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
-//             break;
-//         case "GNU General Public License v3.0":
-//             link = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
-//             break;
-//         case "MIT License":
-//             link = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-//             break;
-//         case "BSD-2-Clause Simplified License":
-//             link = "[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)";
-//             break;
-//         case "Mozilla Public License 2.0":
-//             link = "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)";
-//             break;
-//      };
-
-//     // Return the content string which contains the content of the ReadMe file.
-//     let content = `# ${userResponses.title}
-
-// ${link}
-
-// ## Description
-
-// ${userResponses.description}
-
-// ## Table of Contents
-// * [Installation](#installation)
-// * [Usage](#usage)
-// * [License](#license)
-// * [Contributing](#contributing)
-// * [Tests](#tests)
-// * [Questions](#questions)
-
-// ## Installation
-// ${userResponses.installation};
-
-// ## Usage
-// ${userResponses.usage}
-
-// ## License
-// This application is covered under license: ${userResponses.license}
-
-// ## Contributing
-// ${userResponses.contribution}
-
-// ## Tests
-// ${userResponses.testInstructions}
-
-// ## Questions
-// ### Contact Information:
-
-// GitHub Profile: [@${userResponses.gitHubUserName}](http://github.com/${userResponses.gitHubUserName})
-
-// Email: <${userResponses.email}>
-//     `;
-
-//     return content;
-// }
-
-// USER INTERACTIONS ==========================
-
 
 getManager();
 
